09ebe25e6244e924ad1867a7fa91c34aba09f705
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-07-12 21:59:27 -0600

    finsihed inquire, now moving on to run jest tests on other pages

--- index.js
+++ index.js
@@ -23,34 +23,36 @@ const promptUser = function () {
             type: 'number',
             name: 'office',
             message: 'What is the managers office number?'
-        },
-    ]);
+        }
+    ])
+    .then(menu);
 }
 
 const addIntern = function () {
-    return inquireer.prompt ([
+    return inquirer.prompt ([
         {
             type: 'input',
             name: 'internName',
-            message: 'What is the manager name?',
+            message: 'What is the intern name?',
 
         },
         {
             type: 'number',
             name: 'internID',
-            message: 'what is the manager ID number?'
+            message: 'what is the intern ID number?'
         },
         {
             type: 'input',
             name: 'internEmail',
-            message: 'what is the manager email?'
+            message: 'what is the intern email?'
         },
         {
             type: 'input',
             name: 'school',
             message: 'What is the interns school name?'
         },
-    ]);
+    ])
+    .then(menu);
 }
 
 const addEngineer = function () {
@@ -58,18 +60,18 @@ const addEngineer = function () {
         {
             type: 'input',
             name: 'engineerName',
-            message: 'What is the manager name?',
+            message: 'What is the engineers name?',
 
         },
         {
             type: 'number',
             name: 'engineerID',
-            message: 'what is the manager ID number?'
+            message: 'what is the engineers ID number?'
         },
         {
             type: 'input',
             name: 'engineerEmail',
-            message: 'what is the manager email?'
+            message: 'what is the engineers email?'
         },
         {
             type: 'input',
@@ -77,6 +79,7 @@ const addEngineer = function () {
             message: 'What is the engineers github URL?'
         },
     ])
+    .then(menu);
 }
 
 const menu = function () {
@@ -85,10 +88,27 @@ const menu = function () {
             type: 'list',
             name: 'menu1',
             message: 'Please select an option below to continue making a team or finsih:',
-            choices: ['Add an Intern','Add an Engineer','Finish making a Team']
+            choices: ['Add an Intern','Add an Engineer','Finish making a Team','quit']
         }
     ])
+    .then(responses => {
+        console.log(responses.menu1)
+
+        if (responses.menu1 === 'Add an Intern') {
+            addIntern();
+        }
+        else if (responses.menu1 === 'Add an Engineer') {
+            addEngineer();
+        }
+        else if (responses.menu1 === 'Finish making a Team') {
+            console.log("booty")
+        }
+        else {
+            console.log("please make a decision!")
+        }
+    });
 }
+
 promptUser()
-    .then(menu)
+    
     
